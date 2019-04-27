7d4f7a75b5affe627790cda98469f45de9206f7f
Ahmed Helmy <DevAhmedHelmy@users.noreply.github.com>
2019-04-27 21:01:04 +0200

    Update nav.php

--- app/Template/nav.php
+++ app/Template/nav.php
@@ -130,17 +130,7 @@
                 <a href="/outlay"><i class="fa fa-money"></i> Outlay</a>
             </li>
 
-            <li>
-                <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Dropdown <i class="fa fa-fw fa-caret-down"></i></a>
-                <ul id="demo" class="collapse">
-                    <li>
-                        <a href="#">Dropdown Item</a>
-                    </li>
-                    <li>
-                        <a href="#">Dropdown Item</a>
-                    </li>
-                </ul>
-            </li>
+           
 
         </ul>
     </div>
